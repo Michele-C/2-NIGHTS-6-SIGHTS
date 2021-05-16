c2db1207098e599b15f326e5ecdb8b616c18d941
Michele-C <michele@crestireland.com>
2021-05-16 15:42:17 +0000

    new suitcase button javascrip to add to suit case

---
+++ assets/script.js
@@ -0,0 +1,124 @@
+
+alert("hello from inside new jsfile");
+
+function addItemCliffs(){
+
+    var x = document.createElement("li");
+
+    var button = document.getElementById("selectCliffs");
+     
+    x.innerHTML = button.value;
+
+    button.value = "";
+
+    document.getElementById("michele").appendChild(x);
+}
+
+
+function addItemBurren(){
+
+    var x = document.createElement("li");
+
+    var button = document.getElementById("selectBurren");
+     
+    x.innerHTML = button.value;
+
+    button.value = "";
+
+    document.getElementById("michele").appendChild(x);
+}
+
+function addItemBunratty(){
+
+    var x = document.createElement("li");
+
+    var button = document.getElementById("selectBunratty");
+     
+    x.innerHTML = button.value;
+
+    button.value = "";
+
+    document.getElementById("michele").appendChild(x);
+}
+
+function addItemPerfumery(){
+
+    var x = document.createElement("li");
+
+    var button = document.getElementById("selectPerfumery");
+     
+    x.innerHTML = button.value;
+
+    button.value = "";
+
+    document.getElementById("michele").appendChild(x);
+}
+
+
+function addItemSmoke(){
+
+    var x = document.createElement("li");
+
+    var button = document.getElementById("selectSmoke");
+     
+    x.innerHTML = button.value;
+
+    button.value = "";
+
+    document.getElementById("michele").appendChild(x);
+}
+
+
+function addItemHazel(){
+
+    var x = document.createElement("li");
+
+    var button = document.getElementById("selectHazel");
+     
+    x.innerHTML = button.value;
+
+    button.value = "";
+
+    document.getElementById("michele").appendChild(x);
+}
+
+function addItemDromoland(){
+
+    var x = document.createElement("li");
+
+    var button = document.getElementById("selectDromoland");
+     
+    x.innerHTML = button.value;
+
+    button.value = "";
+
+    document.getElementById("michele").appendChild(x);
+}
+
+function addItemOldground(){
+
+    var x = document.createElement("li");
+
+    var button = document.getElementById("selectOldground");
+     
+    x.innerHTML = button.value;
+
+    button.value = "";
+
+    document.getElementById("michele").appendChild(x);
+}
+
+
+function addItemDooling(){
+
+    var x = document.createElement("li");
+
+    var button = document.getElementById("selectDooling");
+     
+    x.innerHTML = button.value;
+
+    button.value = "";
+
+    document.getElementById("michele").appendChild(x);
+}
+
