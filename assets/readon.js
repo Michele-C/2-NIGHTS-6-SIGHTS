49d561b108f5ea46a0ae252df80fa7d617019757
Michele-C <michele@crestireland.com>
2021-05-17 00:40:19 +0000

    all my logic has been added to this commit html id and headings were changed

---
+++ assets/readon.js
@@ -0,0 +1,43 @@
+$("footer").append("<span>2 Nights | 6 Sights &copy; 2021.</span>")
+
+//Click on the READ ON buttons in the html and the text appears in the DOM
+
+$("#readcliffs").click(function(){
+        $("#readcliffsp").text("One of Ireland’s favourite visitor experiences, the Cliffs of Moher tower over the rugged west Clare coast. Walk the safe, paved pathways and view the famous Cliffs on Europe’s western frontier and enjoy the spectacular vistas over the Atlantic Ocean and the Aran Islands.");
+    });
+
+    $("#readBurren").click(function(){
+        $("#readBurrenp").text("The Burren National Park contains examples of all the major habitats within the Burren: Limestone Pavement, Calcareous Grassland, Hazel scrub, Ash/Hazel Woodland, Turloughs, Lakes, Petrifying Springs, Cliffs and Fen. The highest point is Knockanes(207m) which continues to Mullaghmór to the south. East is a low lying limestone pavement containing a number of semi-permanent lakes. Breath-taking location!")
+    });
+    $("#readBunratty").click(function(){
+        $("#readBunrattyp").text("Bunratty Castle is the most complete and authentic castle in Ireland. The site on which Bunratty Castle stands was in origin a Viking trading camp in 970. The present structure is the last of four castles to be built on the site. Take a tour of this iconic fortress; hear and see how the lords and ladies of this castle lived; hear stories of battles and bravery and enjoy stunning views across the idyllic Clare countryside.")
+
+    });
+
+     $("#readPerfumery").click(function(){
+        $("#readPerfumeryp").text("The Burren Perfumery was established over 30 years ago. It is a family-run company where organic, natural aromatherapy products and fragrances are made. Learn about the traditional methods in producing the cosmetics. Stop by the tea rooms and enjoy the peaceful atmosphere in the heart of the Burren.")
+    });
+
+
+  $("#readSmoke").click(function(){
+        $("#readSmokep").text("The Burren Smokehouse is a family-run business set up by Birgitta & Peter Curtin. The Burren Smokehouse Visitor Centre & Tasting Room is located in Lisdoonvarna just off the Wild Atlantic Way and a short drive from the Cliffs of Moher. Discover the secret of how smoked salmon is handcrafted.")
+    });
+
+  $("#readHazel").click(function(){
+        $("#readHazelp").text("The award winning Chocolatiers located in the North Clare on Athlantic Way is worth a visit. The tea room and shop is amazing. The locally sourced food is delicious. Even Nigella Lawson is a massive fan of their hot chocolate! She swooned upon tasting it!") 
+    });
+
+  $("#readDromo").click(function(){
+        $("#readDromop").text(" dromland castle landscapr")
+    });
+
+  $("#readOldg").click(function(){
+        $("#readOldgp").text("The 4 Star Old Ground Hotel is one of the top hotels in Clare and offers guests a luxury hotel break in a central location in Ennis. Offering excellent award winning hotel accommodation in the heart of Ennis, the award winning Old Ground Hotel is one of the finest luxury hotels in Clare.")
+    });
+    
+      $("#readGlamp").click(function(){
+        $("#readGlampp").text("Doolin Glamping is located in the heart Doolin, north Clare. Established in 2014 on the grounds of O'Connor's Riverside Caravan and Camping Park it blends the experience of being in the outdoors and being one with nature while still hanging on to most of the luxuries of home. One for the list for definite.")
+    });
+        
+             
+              
